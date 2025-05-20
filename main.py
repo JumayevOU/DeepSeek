6e14303eb2ccba94d35d1b456492559a079cdbc5
JumayevOU <jumayevou@gmail.com>
2025-05-21 02:16:53 +0500

    YANGILANDI

---
+++ main.py
@@ -0,0 +1,80 @@
+import os
+from dotenv import load_dotenv  
+load_dotenv()  
+
+import asyncio
+import logging
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+from aiogram.methods import DeleteWebhook
+from aiogram.types import Message
+import aiohttp
+
+TOKEN = os.getenv("BOT_TOKEN")
+AI_TOKEN = os.getenv("API_TOKEN")
+
+logging.basicConfig(level=logging.INFO)
+
+bot = Bot(TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def cmd_start(message: types.Message):
+    await message.answer(
+        "👋 <b>Keling tanishib olaylik!</b>\n\n"
+        "🤖 Men sizning AI yordamchingizman. Quyidagilarni qila olaman:\n"
+        "➤ Matnli savollaringizga javob beraman\n"
+        "➤ Har qanday mavzuda izoh, yechim yoki maslahat bera olaman\n\n"
+        "✍️ Savolingizni yozing men sizga javob berishga harakat qilaman. Boshladikmi?",
+        parse_mode="HTML"
+    )
+
+@dp.message()
+async def process_message(message: Message):
+    loading_msg = await message.answer("🧠 <b>Savolingiz tahlil qilinmoqda...</b>", parse_mode="HTML")
+
+    url = "https://api.intelligence.io.solutions/api/v1/chat/completions"
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {AI_TOKEN}",
+    }
+
+    data = {
+        "model": "deepseek-ai/DeepSeek-R1",
+        "messages": [
+            {
+                "role": "system",
+                "content": "Siz foydalanuvchiga foydali javoblar beradigan yordamchisiz.",
+            },
+            {
+                "role": "user",
+                "content": message.text
+            }
+        ],
+    }
+
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(url, headers=headers, json=data) as response:
+                response.raise_for_status()
+                result = await response.json()
+
+                text = result['choices'][0]['message']['content']
+                if '</think>' in text:
+                    javob = text.split('</think>\n\n')[1]
+                else:
+                    javob = text
+
+                await bot.delete_message(chat_id=message.chat.id, message_id=loading_msg.message_id)
+                await message.answer(javob, parse_mode="Markdown")
+        except Exception as e:
+            await bot.delete_message(chat_id=message.chat.id, message_id=loading_msg.message_id)
+            await message.answer("❌ Kechirasiz, javob olishda xatolik yuz berdi.")
+            print("Xatolik:", e)
+
+async def main():
+    await bot(DeleteWebhook(drop_pending_updates=True))
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
